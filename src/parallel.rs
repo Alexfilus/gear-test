1a6266800ed83d0257db5ba45f4d8383233046b0
alexfilus <alexfilus@gmail.com>
2023-10-30 20:23:39 +0400

    rewrite without Clone T

--- src/parallel.rs
+++ src/parallel.rs
@@ -3,9 +3,9 @@ use std::thread;
 
 pub fn parallel_computation<T, R, F>(data: Vec<T>, func: F, threshold: usize) -> Vec<R>
     where
-        T: Send + Debug + Clone + 'static,
+        T: Send + Debug + 'static,
         R: Send + 'static,
-        F: Fn(T) -> R + Send + Clone + 'static,
+        F: FnMut(T) -> R + Send + Clone + 'static,
 {
     let data_len = data.len();
 
@@ -14,29 +14,30 @@ pub fn parallel_computation<T, R, F>(data: Vec<T>, func: F, threshold: usize) ->
     }
 
     let num_threads = 4;
+
     let chunk_size = data_len / num_threads;
 
     let mut handles = vec![];
-    let mut start = 0;
+
     let mut remainder = data_len % num_threads;
+    let mut remaining_data = data;
+
     for _ in 0..num_threads {
-        let mut end = start + chunk_size;
+        let mut end = chunk_size;
         if remainder > 0 {
             end += 1;
             remainder -= 1;
         }
-
-        let chunk: Vec<T> = data[start..end].to_vec();
+        let chunk = remaining_data.split_off(end);
         let func_clone = func.clone();
 
         let handle = thread::spawn(move || {
-            chunk.into_iter().map(func_clone).collect::<Vec<R>>()
+            remaining_data.into_iter().map(func_clone).collect::<Vec<R>>()
         });
         handles.push(handle);
 
-        start = end;
+        remaining_data = chunk;
     }
-
     let mut result = Vec::with_capacity(data_len);
 
     for handle in handles {
