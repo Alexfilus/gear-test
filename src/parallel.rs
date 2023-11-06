bbd12a93434dd78bdf1b84f6c5d130ee6299f6a3
alexfilus <alexfilus@gmail.com>
2023-11-06 13:20:24 +0400

    with rayon

--- src/parallel.rs
+++ src/parallel.rs
@@ -1,49 +1,14 @@
-use std::fmt::Debug;
-use std::thread;
+use rayon::prelude::*;
 
-pub fn parallel_computation<T, R, F>(data: Vec<T>, func: F, threshold: usize) -> Vec<R>
+pub fn parallel_computation<T, R, F>(input: Vec<T>, function: F, threshold: usize) -> Vec<R>
     where
-        T: Send + Debug + 'static,
-        R: Send + 'static,
-        F: FnMut(T) -> R + Send + Clone + 'static,
+        T: Send,
+        R: Send + Default,
+        F: Fn(T) -> R + Sync + Send,
 {
-    let data_len = data.len();
-
-    if data_len < threshold {
-        return data.into_iter().map(func).collect();
+    if input.len() < threshold {
+        input.into_iter().map(function).collect()
+    } else {
+        input.into_par_iter().map(function).collect()
     }
-
-    let num_threads = 4;
-
-    let chunk_size = data_len / num_threads;
-
-    let mut handles = vec![];
-
-    let mut remainder = data_len % num_threads;
-    let mut remaining_data = data;
-
-    for _ in 0..num_threads {
-        let mut end = chunk_size;
-        if remainder > 0 {
-            end += 1;
-            remainder -= 1;
-        }
-        let chunk = remaining_data.split_off(end);
-        let func_clone = func.clone();
-
-        let handle = thread::spawn(move || {
-            remaining_data.into_iter().map(func_clone).collect::<Vec<R>>()
-        });
-        handles.push(handle);
-
-        remaining_data = chunk;
-    }
-    let mut result = Vec::with_capacity(data_len);
-
-    for handle in handles {
-        result.extend(handle.join().unwrap());
-    }
-
-    result
 }
-
